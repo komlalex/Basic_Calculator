24fe39c09c9748bc4b6204eb81a63a869b6ea0b1
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 11:59:08 +0000

    Final commit

--- calculator.py
+++ calculator.py
@@ -19,3 +19,44 @@ def mul(a, b):
 def div(a, b):
     answer = a / b
     print(f"a ÷ b = {answer}")
+
+print("Welcome to Kalculator")
+
+while True:
+    print("What operation do you want to perform: ")
+    print("A -> Addition")
+    print("B -> Subtraction")
+    print("C -> Multiplication")
+    print("D -> Division")
+    print("E -> Exit")
+
+    choice = str(input("Input your choice: \n"))
+
+    choice = choice.lower()
+
+    if choice == "a" :
+        print("Addition")
+        a = int(input("First number: \n"))
+        b = int(input("Second number: \n"))
+        add(a, b)
+    elif choice == "b":
+        print("Subtraction")
+        a = int(input("First number: \n"))
+        b = int(input('Second number: \n'))
+        sub(a, b)
+    elif choice == "c":
+        print("Multiplication")
+        a = int(input("First number: \n"))
+        b = int(input("Second number: \n"))
+        mul(a, b)
+    elif choice == "d":
+        print("Division")
+        a = int(input("First number: \n"))
+        b = int(input("Second number: \n"))
+        div(a, b)
+    elif choice == "e":
+        print("Thank you for using Kalculator.")
+        quit()
+    else:
+        print(f"{choice} is not a valid option.")
+
