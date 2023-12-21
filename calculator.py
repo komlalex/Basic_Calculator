62b125250e0ee8d2e4a1954accc7dbc2832967b0
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 09:55:19 +0000

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Define the functions needed: add, sub, mul, div
+# print options to the user
+# ask for values
+# call the functions 
+# while loop to continue the orogram until the user wants to exit
+
+def add(a, b):
+    answer = a + b
+    print(f"a + b = {answer}")
+
+def sub(a, b):
+    answer = a - b
+    print(f"a - b = {answer}")
+
+def mul(a, b):
+    answer = a * b
+    print(f"a x b = {answer}")
+
+def div(a, b):
+    answer = a / b
+    print(f"a ÷ b = {answer}")
